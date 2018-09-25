e3e82c4460b7824776f568c80421a34383f8d075
Jacek Smolak <jacek.smolak@gmail.com>
2018-09-25 08:00:45 +0200

    Setup tests to use chai-as-promised

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,8 +1,10 @@
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
+import chaiAsPromised from 'chai-as-promised';
 
 import throwOnWarnings from './helpers/throwOnWarnings';
 
 throwOnWarnings(console);
 
 chai.use(sinonChai);
+chai.use(chaiAsPromised);
