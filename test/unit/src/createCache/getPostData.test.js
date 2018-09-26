1f66303eb4bf70207b56d98db3c3b8bad2b326fa
Jacek Smolak <jacek.smolak@gmail.com>
2018-09-26 16:59:42 +0200

    Promisify getPostData

--- test/unit/src/createCache/getPostData.test.js
+++ test/unit/src/createCache/getPostData.test.js
@@ -1,24 +1,25 @@
-import sinon from 'sinon';
 import { expect } from 'chai';
+import sinon from 'sinon';
 import { nonObjectValues, nonStringValues, createDummyAdapter } from 'stash-it-test-helpers';
 
-import { createCache, getPostData } from '../../../../src/createCache';
+import {createCache, getPostData} from '../../../../src/createCache';
 import createItem from '../../../../src/createItem';
 
 describe('getPostData', () => {
-    let cache;
+    let cacheInstance;
     let dummyAdapter;
+    let anyValidArgs;
 
     beforeEach(() => {
         dummyAdapter = createDummyAdapter(createItem);
-        cache = createCache(dummyAdapter);
+        cacheInstance = createCache(dummyAdapter);
+        anyValidArgs = { cacheInstance };
     });
 
     context('when method name is not passed as a string', () => {
         it('should throw', () => {
             nonStringValues.forEach((methodName) => {
-                expect(getPostData.bind(null, methodName))
-                    .to.throw("'methodName' must be a string.");
+                expect(getPostData.bind(null, methodName)).to.throw("'methodName' must be a string.");
             });
         });
     });
@@ -38,70 +39,101 @@ describe('getPostData', () => {
         });
     });
 
-    it('should return object with the same keys as passed with cacheInstance as an additional one', () => {
-        const args = { cacheInstance: cache, foo: 'bar' };
-        const preData = getPostData('someMethodName', args);
-        const keys = Object.keys(preData);
-        const expectedKeys = [ 'cacheInstance', 'foo' ];
+    it('should return object with the same keys that args were passed with (as values can differ)', async () => {
+        const postData = await getPostData('someMethodName', anyValidArgs);
+        const keys = Object.keys(postData);
+        const expectedKeys = [ 'cacheInstance' ];
 
         expect(keys).to.deep.eq(expectedKeys);
     });
 
-    it('should return reference to cache instance under cacheInstance property', () => {
-        const handler = () => {};
+    describe('returned cacheInstance', () => {
+        it('should return reference to cacheInstance instance under cacheInstance property', async () => {
+            const postData = await getPostData('someMethodName', anyValidArgs);
 
-        cache.addHook({ event: 'preSomething', handler });
+            expect(postData.cacheInstance).to.not.be.undefined;
+        });
 
-        const args = { cacheInstance: cache, foo: 'bar' };
-        const preData = getPostData('someMethodName', args);
-        const cacheInstance = preData.cacheInstance;
-        const expectedHooks = {
-            preSomething: [
-                handler
-            ]
-        };
+        it('should return reference to cacheInstance', async () => {
+            const postData = await getPostData('someMethodName', anyValidArgs);
 
-        expect(cacheInstance === cache).to.be.true;
-        expect(cacheInstance).to.deep.equal(cache);
-        expect(cacheInstance.getHooks()).to.deep.equal(expectedHooks);
+            expect(postData.cacheInstance === cacheInstance).to.be.true;
+        });
     });
 
     context('when there is no hook for given event', () => {
-        it('should return args in an exact form as they were passed in the first place', () => {
-            const args = { foo: 'bar', cacheInstance: cache };
-            const spy = sinon.spy();
-            const hook = {
-                event: 'postSomeOtherEventName',
-                handler: spy
-            };
+        it('should return args in an exact form as they were passed in the first place', async () => {
+            cacheInstance.addHook({ event: 'postSomeOtherEventName', handler: () => {} });
 
-            cache.addHook(hook);
+            const postData = await getPostData('eventName', anyValidArgs);
 
-            const returnedArgs = getPostData('eventName', args);
+            expect(postData === anyValidArgs).to.be.true;
+        });
 
-            expect(returnedArgs === args).to.be.true;
-            expect(returnedArgs).to.deep.equal(args);
-            expect(spy).to.not.have.been.called;
+        it('should not pass args through hooks that were registered for that event', async () => {
+            const hook = { event: 'postSomeOtherEventName', handler: sinon.spy() };
+
+            cacheInstance.addHook(hook);
+
+            await getPostData('eventName', anyValidArgs);
+
+            expect(hook.handler).to.not.have.been.called;
         });
     });
 
     context('when there is a hook for given event', () => {
-        it(`should return args handled by that hook's handler (whatever it does)`, () => {
-            const args = { foo: 'bar', cacheInstance: cache };
-            const stub = sinon.stub().returnsArg(0);
-            const hook = {
-                event: 'postEventName',
-                handler: stub
-            };
+        it(`should return args handled by that hook's handler (whatever it does)`, async () => {
+            const identityStub = sinon.stub().returnsArg(0);
 
-            cache.addHook(hook);
+            cacheInstance.addHook({ event: 'postEventName', handler: identityStub });
 
-            const returnedArgs = getPostData('eventName', args);
+            const postData = await getPostData('eventName', anyValidArgs);
 
-            expect(returnedArgs).to.deep.equal(args);
-            expect(stub)
-                .to.have.been.calledWith(args)
-                .to.have.been.calledOnce;
+            expect(postData).to.deep.equal(anyValidArgs);
+            expect(identityStub).to.have.been.calledWith(anyValidArgs).to.have.been.calledOnce;
+        });
+    });
+
+    describe('executing handlers', () => {
+        context('for synchronous handlers', () => {
+            it('should happen in sequence', async () => {
+                const hook1 = {
+                    event: 'postEventName',
+                    handler: sinon.spy()
+                };
+                const hook2 = {
+                    event: 'postEventName',
+                    handler: sinon.spy()
+                };
+
+                cacheInstance.addHooks([ hook1, hook2 ]);
+                await getPostData('eventName', anyValidArgs);
+
+                expect(hook1.handler).to.have.been.calledBefore(hook2.handler);
+            });
+        });
+
+        context('for asynchronous handlers', () => {
+            it('should happen in sequence', async () => {
+                const stallFor = async (time) => await new Promise(resolve => setTimeout(resolve, time));
+                const spyForSlowHandler = sinon.spy();
+                const hook1 = {
+                    event: 'postEventName',
+                    handler: async () => {
+                        await stallFor(50);
+                        spyForSlowHandler();
+                    }
+                };
+                const hook2 = {
+                    event: 'postEventName',
+                    handler: sinon.spy()
+                };
+
+                cacheInstance.addHooks([ hook1, hook2 ]);
+                await getPostData('eventName', anyValidArgs);
+
+                expect(spyForSlowHandler).to.have.been.calledBefore(hook2.handler);
+            });
         });
     });
 });
