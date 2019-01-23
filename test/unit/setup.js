f01a466a1df7a5d15b091b737f618df6b06a13b2
Jacek Smolak <jacek.smolak@gmail.com>
2019-01-23 19:19:45 +0100

    Uninstall chai-as-promised
    It's not used anymore.

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,10 +1,8 @@
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
-import chaiAsPromised from 'chai-as-promised';
 
 import throwOnWarnings from './helpers/throwOnWarnings';
 
 throwOnWarnings(console);
 
 chai.use(sinonChai);
-chai.use(chaiAsPromised);
